6dc6ad528b75014e62e98c278daa23c0e8e8805b
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-01-18 19:57:38 +0900

    jdbc-2

--- jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
+++ jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -9,10 +9,11 @@ import java.sql.SQLException;
 import kr.or.connect.jdbcexam.dto.Role;
 
 public class RoleDao {
+	private static String dbUrl = "jdbc:mysql://localhost:3306/connectdb?serverTimezone=UTC";
+	private static String dbUser = "connectuser";
+	private static String dbpasswd = "connect123!@#";
+	
 	public Role getRole(Integer roleId) {
-		String dbUrl = "jdbc:mysql://localhost:3306/connectdb?serverTimezone=UTC";
-		String dbUser = "connectuser";
-		String dbpasswd = "connect123!@#";
 		
 		Role role = null;
 		Connection conn = null;
@@ -61,5 +62,44 @@ public class RoleDao {
 		}
 		
 		return role;
+	}
+	
+	public int addRole(Role role) {
+		int insertCount = 0;
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbUrl, dbUser, dbpasswd);
+			String sql = "INSERT INTO role (role_id, description) VALUES (?, ?)";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1,role.getRoleId());
+			ps.setString(2, role.getDescription());
+			
+			insertCount = ps.executeUpdate();
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(conn!=null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return insertCount;
 	}
 }
