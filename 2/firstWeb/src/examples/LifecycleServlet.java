d071d74e7a35dbfc2839af429fade6ca10834bd9
chae09945ss <stat_chae0@sookmyung.ac.kr>
2021-02-02 20:05:03 +0900

    lifecycle servlet2

--- 2/firstWeb/src/examples/LifecycleServlet.java
+++ 2/firstWeb/src/examples/LifecycleServlet.java
@@ -25,6 +25,15 @@ public class LifecycleServlet extends HttpServlet {
 	}
 
 	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		String name = req.getParameter("name");
+		out.println("<h1> hello " + name + "</h1>");
+		out.close();
+	}
+
 	public void destroy() {
 		System.out.println("destroy 호출!!");
 	}
@@ -32,6 +41,17 @@ public class LifecycleServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
        {
-		System.out.println("service호출!!");		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstWeb/LifecycleServlet'>");
+		out.println("name : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");                                                 
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();	
 	}
 }
